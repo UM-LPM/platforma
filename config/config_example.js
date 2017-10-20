b8c8aaa8f2de2fbb605532f68aeab83a6f7a265b
Ravby <ravby69@gmail.com>
2017-10-20 14:12:36 +0200

    deleted config files

--- config/config_example.js
+++
@@ -1,6 +0,0 @@
-exports.GoogleClientId = 'YourGoogleClientId';
-exports.GoogleClientSecret = "YourGoogleClientSecret";
-exports.RedirectionUrl = "yourAppRedirectUrlForGoogle";
-exports.defaultUrl = "yourBaseURL";
-exports.setLanguage = "defaultLanguage";
-exports.setLocale = "defaultLocale";
