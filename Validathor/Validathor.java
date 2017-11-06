89f0f3b05893eb560b315bdca12e495dab045069
Jure Skorc <jure.skorc@gmail.com>
2017-11-07 00:15:26 +0100

    fix

--- Validathor/Validathor.java
+++ Validathor/Validathor.java
@@ -30,8 +30,10 @@ public class Validathor {
 		if (args.length == 2) {
 			String targetFile = args[1];
 			String userAlgorithmFolder = new File(args[0]).getParentFile().toString();
-			String earsPath = new File(args[0]).getParentFile().getParentFile().getParentFile().getParentFile().toString()+"/EARS/ears.jar";
+			String earsPath = new File(args[0]).getParentFile().getParentFile().getParentFile().getParentFile().getParentFile().toString()+"/EARS/ears.jar";
 			String userAlgorithmFilename = new File(args[0]).getName().toString();
+			
+			System.out.println(earsPath);
 			
 			try {
 			ProcessBuilder pb = new ProcessBuilder("javac","-sourcepath",userAlgorithmFolder,"-cp",earsPath,userAlgorithmFilename);
