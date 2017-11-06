b6a49f13ffc7b2edf198fa412220865931f8060c
Jure Skorc <jure.skorc@gmail.com>
2017-11-06 23:28:24 +0100

    reworked Validathor
    now it includes EARS classpath :)

--- Validathor/Validathor.java
+++ Validathor/Validathor.java
@@ -1,4 +1,3 @@
-
 /******************************************************************************
  *  Compilation:  javac Validathor.java
  *  Execution:    java Validathor [programyouwanttocompile.java] [path_to_output_directory]
@@ -13,55 +12,51 @@ import java.io.*;
 public class Validathor {
 
 	private static PrintWriter writer;
-	private static String errors = "";;
+	private static StringBuilder errors;
 
-	private static void printLines(String name, InputStream ins) throws Exception {
-		String line = null;
-		BufferedReader in = new BufferedReader(new InputStreamReader(ins));
-
-		while ((line = in.readLine()) != null) {
-			line = line.replace("\\", "/");
-			errors += "\"" + line.replaceAll("\"", "\\\\\"") + "\",";
+	
+	private static void startProcess(ProcessBuilder pb) throws IOException {
+		Process p = pb.start();
+		BufferedReader stdError = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+		String s = null;
+		StringBuilder sb = new StringBuilder();
+		while ((s = stdError.readLine()) != null) {
+			errors.append(s);
 		}
 	}
 
-	private static void runProcess(String command) throws Exception {
-		Process pro = Runtime.getRuntime().exec(command);
-		//printLines(command + " stdout:", pro.getInputStream());
-		printLines("stderr:", pro.getErrorStream());
-		pro.waitFor();
-	}
-
 	public static void main(String[] args) {
+		errors = new StringBuilder();
 		if (args.length == 2) {
-			File targetFile = new File(args[1]);
-			File parent = targetFile.getParentFile();
-
+			String targetFile = args[1];
+			String userAlgorithmFolder = new File(args[0]).getParentFile().toString();
+			String earsPath = new File(args[0]).getParentFile().getParentFile().getParentFile().getParentFile().toString()+"/EARS/ears.jar";
+			String userAlgorithmFilename = new File(args[0]).getName().toString();
+			
 			try {
-				writer = new PrintWriter(targetFile, "UTF-8");
-			} catch (IOException io) {
-				System.out.println("Error creating report file: " + args[1]);
+			ProcessBuilder pb = new ProcessBuilder("javac","-sourcepath",userAlgorithmFolder,"-cp",earsPath,userAlgorithmFilename);
+
+			pb.directory(new File(userAlgorithmFolder));
+			startProcess(pb);
+
+			} catch (Exception e) {
+				e.printStackTrace();
 				return;
 			}
 
-			try {
-				runProcess("javac -d " + args[0].substring(0, args[0].lastIndexOf('/')) + " " + args[0]);
-				runProcess("java -cp " + args[0].substring(0, args[0].lastIndexOf('/')) + " "
-						+ args[0].substring(args[0].lastIndexOf('/') + 1, args[0].length() - 5));
-				if (errors.length() > 0) {
-					errors = errors.substring(0, errors.length() - 1);
-					errors = "{\"error_list\": [" + errors + "] }";
-					writer.println(errors);
-				} else {
-					errors = "{\"error_list\": [] }";
-					writer.println(errors);
-				}
 
-				errors = null;
+			try {
+			
+				ProcessBuilder pb;
+				pb = new ProcessBuilder("java","-cp",earsPath+File.pathSeparator+userAlgorithmFolder+"/",userAlgorithmFilename.substring(0, userAlgorithmFilename.lastIndexOf(".")));
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
-			writer.close();
+			
+			try( PrintWriter out = new PrintWriter(targetFile) )
+			{
+				out.println("{\"error_list\": \""+errors.toString().replaceAll("\"","\\\\\"")+"\"}");
+			} catch(Exception b) {}
 		} else
 			System.out.println(
 					"Error, invalid startup. Run the program with command: \njava Validathor [programyouwanttocompile.java] [path_to_output_directory]");
